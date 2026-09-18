f1d150dbce857cd7a50c609c1db47c1fa1069ff5
Ryan Eaton <ryaneaton@spea.cc>
2026-09-18 10:30:34 -0400

    gametest: add conductor goggles/linking, semaphore connection, and moving train tests
    (cherry picked from commit 309d0bc21fbecc47efae4041941a0f83d06b0806)

--- fabric/src/main/java/com/railwayteam/railways/gametest/ConductorGameTests.java
+++ fabric/src/main/java/com/railwayteam/railways/gametest/ConductorGameTests.java
@@ -24,13 +24,25 @@ import com.railwayteam.railways.registry.CRBlocks;
 import com.railwayteam.railways.registry.CREntities;
 import com.railwayteam.railways.registry.CRItems;
 import com.railwayteam.railways.registry.CRTags;
+import com.zurrtum.create.AllBlocks;
 import com.zurrtum.create.AllItems;
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class ConductorGameTests {
 
@@ -84,6 +96,91 @@ public class ConductorGameTests {
             ItemStack cap = CRItems.ITEM_CONDUCTOR_CAP.get(color).asStack();
             helper.assertTrue(CRTags.AllItemTags.CONDUCTOR_CAPS.matches(cap), "Cap item for " + color + " must match tag");
         }
+        helper.succeed();
+    }
+
+    @GameTest(structure = RailwaysGameTestHelper.PLATFORM_8X8)
+    public void testConductorCapConvertsCasing(GameTestHelper helper) {
+        BlockPos localPos = new BlockPos(2, 1, 2);
+        BlockPos absPos = helper.absolutePos(localPos);
+        helper.setBlock(localPos, AllBlocks.ANDESITE_CASING.defaultBlockState());
+        helper.assertBlockPresent(AllBlocks.ANDESITE_CASING, localPos);
+
+        Player player = helper.makeMockPlayer(GameType.SURVIVAL);
+        ItemStack redCap = CRItems.ITEM_CONDUCTOR_CAP.get(DyeColor.RED).asStack();
+        player.setItemInHand(InteractionHand.MAIN_HAND, redCap);
+
+        BlockHitResult hitResult = new BlockHitResult(Vec3.atCenterOf(absPos), Direction.UP, absPos, false);
+        UseOnContext context = new UseOnContext(player, InteractionHand.MAIN_HAND, hitResult);
+        InteractionResult result = redCap.useOn(context);
+
+        helper.assertTrue(result.consumesAction(), "Cap use on casing should consume action");
+        helper.assertBlockNotPresent(AllBlocks.ANDESITE_CASING, localPos);
+        helper.assertEntityPresent(CREntities.CONDUCTOR.get(), localPos);
+        helper.succeed();
+    }
+
+    @GameTest(structure = RailwaysGameTestHelper.PLATFORM_8X8)
+    public void testConductorGogglesSpyJob(GameTestHelper helper) {
+        BlockPos pos = new BlockPos(3, 1, 3);
+        ConductorEntity conductor = helper.spawn(CREntities.CONDUCTOR.get(), pos);
+        helper.assertTrue(conductor.getJob() == ConductorEntity.Job.DEFAULT, "Initial conductor job should be DEFAULT");
+
+        Player player = helper.makeMockPlayer(GameType.SURVIVAL);
+        player.setItemInHand(InteractionHand.MAIN_HAND, new ItemStack(AllItems.GOGGLES));
+
+        InteractionResult result = player.interactOn(conductor, InteractionHand.MAIN_HAND);
+        helper.assertTrue(result.consumesAction(), "Giving goggles to conductor should succeed");
+        helper.assertTrue(conductor.getJob() == ConductorEntity.Job.SPY, "Conductor job should become SPY");
+        helper.assertTrue(conductor.getSecondaryHeadStack().is(AllItems.GOGGLES), "Conductor secondary head stack should match AllItems.GOGGLES");
+
+        // Shift-interact with empty hand retrieves goggles
+        player.setShiftKeyDown(true);
+        player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
+        player.interactOn(conductor, InteractionHand.MAIN_HAND);
+        helper.assertTrue(conductor.getJob() == ConductorEntity.Job.DEFAULT, "Conductor job should revert to DEFAULT");
+        helper.assertTrue(player.getItemInHand(InteractionHand.MAIN_HAND).is(AllItems.GOGGLES), "Player should have retrieved the goggles");
+        helper.succeed();
+    }
+
+    @GameTest(structure = RailwaysGameTestHelper.PLATFORM_8X8)
+    public void testConductorRedstoneLinkJob(GameTestHelper helper) {
+        BlockPos pos = new BlockPos(3, 1, 3);
+        ConductorEntity conductor = helper.spawn(CREntities.CONDUCTOR.get(), pos);
+        helper.assertTrue(conductor.getJob() == ConductorEntity.Job.DEFAULT, "Initial conductor job should be DEFAULT");
+
+        Player player = helper.makeMockPlayer(GameType.SURVIVAL);
+        player.setItemInHand(InteractionHand.MAIN_HAND, new ItemStack(AllBlocks.REDSTONE_LINK));
+
+        InteractionResult result = player.interactOn(conductor, InteractionHand.MAIN_HAND);
+        helper.assertTrue(result.consumesAction(), "Giving redstone link to conductor should succeed");
+        helper.assertTrue(conductor.getJob() == ConductorEntity.Job.REMOTE_CONTROL, "Conductor job should become REMOTE_CONTROL");
+
+        // Shift-interact with empty hand retrieves redstone link
+        player.setShiftKeyDown(true);
+        player.setItemInHand(InteractionHand.MAIN_HAND, ItemStack.EMPTY);
+        player.interactOn(conductor, InteractionHand.MAIN_HAND);
+        helper.assertTrue(conductor.getJob() == ConductorEntity.Job.DEFAULT, "Conductor job should revert to DEFAULT");
+        helper.assertTrue(player.getItemInHand(InteractionHand.MAIN_HAND).is(AllBlocks.REDSTONE_LINK.asItem()), "Player should have retrieved redstone link");
+        helper.succeed();
+    }
+
+    @GameTest(structure = RailwaysGameTestHelper.PLATFORM_8X8)
+    public void testRemoteLensBinding(GameTestHelper helper) {
+        BlockPos pos = new BlockPos(3, 1, 3);
+        ConductorEntity conductor = helper.spawn(CREntities.CONDUCTOR.get(), pos);
+        conductor.setJob(ConductorEntity.Job.SPY);
+
+        Player player = helper.makeMockPlayer(GameType.SURVIVAL);
+        ItemStack lensStack = new ItemStack(CRItems.REMOTE_LENS.get());
+        player.setItemInHand(InteractionHand.MAIN_HAND, lensStack);
+
+        InteractionResult result = CRItems.REMOTE_LENS.get().interactLivingEntity(lensStack, player, conductor, InteractionHand.MAIN_HAND);
+        helper.assertTrue(result == InteractionResult.SUCCESS, "Remote lens should successfully bind to SPY conductor");
+
+        CompoundTag tag = player.getItemInHand(InteractionHand.MAIN_HAND).getOrDefault(DataComponents.CUSTOM_DATA, CustomData.EMPTY).copyTag();
+        helper.assertTrue(tag.contains("SelectedConductor"), "Custom data should contain SelectedConductor");
+        helper.assertTrue(tag.getStringOr("SelectedConductor", "").equals(conductor.getUUID().toString()), "SelectedConductor must match conductor UUID");
         helper.succeed();
     }
 }
--- fabric/src/main/java/com/railwayteam/railways/gametest/LocomotiveGameTests.java
+++ fabric/src/main/java/com/railwayteam/railways/gametest/LocomotiveGameTests.java
@@ -19,9 +19,26 @@
 package com.railwayteam.railways.gametest;
 
 import com.railwayteam.railways.registry.CRBlocks;
+import com.zurrtum.create.AllBlocks;
+import com.zurrtum.create.Create;
+import com.zurrtum.create.content.trains.entity.CarriageBogey;
+import com.zurrtum.create.content.trains.entity.Train;
+import com.zurrtum.create.content.trains.track.TrackBlock;
+import com.zurrtum.create.content.trains.track.TrackPropagator;
+import com.zurrtum.create.content.trains.track.TrackShape;
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 public class LocomotiveGameTests {
 
@@ -59,40 +76,49 @@ public class LocomotiveGameTests {
     }
 
     @GameTest(structure = RailwaysGameTestHelper.PLATFORM_8X8)
-    public void testSmokestackExtender(GameTestHelper helper) {
-        BlockPos basePos = new BlockPos(3, 1, 3);
-        BlockPos extPos = new BlockPos(3, 2, 3);
+    public void testTrainMovement(GameTestHelper helper) {
+        BlockState trackState = AllBlocks.TRACK.defaultBlockState()
+                .setValue(TrackBlock.SHAPE, TrackShape.ZO);
 
-        helper.setBlock(basePos, CRBlocks.COALBURNER_STACKS.getFirst().get().defaultBlockState());
-        helper.setBlock(extPos, CRBlocks.COALBURNER_STACKS.getSecond().get().defaultBlockState());
+        for (int z = 1; z <= 6; z++) {
+            BlockPos trackPos = new BlockPos(3, 1, z);
+            helper.setBlock(trackPos, trackState);
+            BlockPos absPos = helper.absolutePos(trackPos);
+            TrackPropagator.onRailAdded(helper.getLevel(), absPos, helper.getBlockState(trackPos));
+        }
 
-        helper.assertBlockPresent(CRBlocks.COALBURNER_STACKS.getFirst().get(), basePos);
-        helper.assertBlockPresent(CRBlocks.COALBURNER_STACKS.getSecond().get(), extPos);
-        helper.succeed();
-    }
+        BlockPos placePos = new BlockPos(3, 1, 3);
+        BlockPos absPlacePos = helper.absolutePos(placePos);
 
-    @GameTest(structure = RailwaysGameTestHelper.PLATFORM_8X8)
-    public void testCustomBogeyBlocks(GameTestHelper helper) {
-        BlockPos p1 = new BlockPos(1, 1, 1);
-        BlockPos p2 = new BlockPos(2, 1, 1);
-        BlockPos p3 = new BlockPos(3, 1, 1);
-        BlockPos p4 = new BlockPos(4, 1, 1);
-        BlockPos p5 = new BlockPos(5, 1, 1);
-        BlockPos p6 = new BlockPos(6, 1, 1);
+        Player player = helper.makeMockPlayer(GameType.SURVIVAL);
+        ItemStack handcarStack = new ItemStack(CRBlocks.HANDCAR.get());
+        player.setItemInHand(InteractionHand.MAIN_HAND, handcarStack);
 
-        helper.setBlock(p1, CRBlocks.SINGLEAXLE_BOGEY.get().defaultBlockState());
-        helper.setBlock(p2, CRBlocks.DOUBLEAXLE_BOGEY.get().defaultBlockState());
-        helper.setBlock(p3, CRBlocks.MONO_BOGEY.get().defaultBlockState());
-        helper.setBlock(p4, CRBlocks.INVISIBLE_BOGEY.get().defaultBlockState());
-        helper.setBlock(p5, CRBlocks.WIDE_DOUBLEAXLE_BOGEY.get().defaultBlockState());
-        helper.setBlock(p6, CRBlocks.NARROW_SMALL_BOGEY.get().defaultBlockState());
+        BlockHitResult hitResult = new BlockHitResult(Vec3.atCenterOf(absPlacePos), Direction.UP, absPlacePos, false);
+        UseOnContext context = new UseOnContext(player, InteractionHand.MAIN_HAND, hitResult);
 
-        helper.assertBlockPresent(CRBlocks.SINGLEAXLE_BOGEY.get(), p1);
-        helper.assertBlockPresent(CRBlocks.DOUBLEAXLE_BOGEY.get(), p2);
-        helper.assertBlockPresent(CRBlocks.MONO_BOGEY.get(), p3);
-        helper.assertBlockPresent(CRBlocks.INVISIBLE_BOGEY.get(), p4);
-        helper.assertBlockPresent(CRBlocks.WIDE_DOUBLEAXLE_BOGEY.get(), p5);
-        helper.assertBlockPresent(CRBlocks.NARROW_SMALL_BOGEY.get(), p6);
-        helper.succeed();
+        InteractionResult placeResult = handcarStack.useOn(context);
+        helper.assertTrue(placeResult.consumesAction(), "Handcar should be successfully placed on the track");
+
+        Train train = null;
+        for (Train t : Create.RAILWAYS.trains.values()) {
+            if (t.owner.equals(player.getUUID())) {
+                train = t;
+                break;
+            }
+        }
+        helper.assertFalse(train == null, "A train should be registered for the player");
+
+        CarriageBogey bogey = train.carriages.get(0).bogeys.getFirst();
+        double initialPos = bogey.leading().position;
+        train.speed = 0.5;
+
+        Train finalTrain = train;
+        helper.succeedWhen(() -> {
+            finalTrain.tick(helper.getLevel());
+            double currentPos = bogey.leading().position;
+            helper.assertTrue(currentPos != initialPos, "Train travelling point position should advance as train moves");
+            Create.RAILWAYS.removeTrain(finalTrain.id);
+        });
     }
 }
